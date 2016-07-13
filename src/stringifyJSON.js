d258c031603b720121db1e5e7c4b180ca90bdc0e
Gamerlazer <gamerlazer@gmail.com>
2016-07-13 02:57:06 -0700

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,47 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var stringify = '';
+  // Check for primitives
+  if (typeof(obj) === 'number') {
+  	stringify += obj;
+  } else if (typeof(obj) === 'boolean') {
+  	stringify += obj;
+  } else if (typeof(obj) === 'string') {
+	return '"' + obj + '"';
+  } else if (typeof(obj) === 'function') {
+  	return null;
+  } else if (obj === null) {
+  	stringify += null;
+  } else if (Array.isArray(obj)) {
+  	for (var i = 0; i < obj.length; i++) {
+  	  if (i === obj.length - 1) {
+  	    stringify += stringifyJSON(obj[i]);
+  	  } else {
+  	  stringify += stringifyJSON(obj[i]) + ',';
+  	  }
+  	}
+  	return '[' + stringify+ ']';
+  } else if (typeof(obj) === 'object') {
+  	for (key in obj) {
+  	  if (typeof(obj[key]) === 'function') {
+  	  	stringify += '';
+  	  } else if (obj[key] === undefined) {
+  	  	stringify += '';
+  	  } else {
+  	    stringify += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+  	  }
+  	}
+  	return '{' + stringify.slice(0,-1) + '}';
+  }
+  return stringify;
 };
+
+  // Check if number, boolean, or string
+  // if number or boolean add to string
+  // if string, convert to string
+  // if array, convert to string array
+  // if object, call function on self 
+  //return final string
+
+
